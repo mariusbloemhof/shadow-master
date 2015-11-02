161d66884ac380f5686138f3e4759ebef5979ed4
Marius Bloemhof <mariusbloemhof@gmail.com>
2015-11-02 20:53:37 +0200

    Implemented Facebook login (partially)

--- app/src/main/java/com/shadow/beast/shadow_master/LoginFragment.java
+++ app/src/main/java/com/shadow/beast/shadow_master/LoginFragment.java
@@ -2,18 +2,47 @@ package com.shadow.beast.shadow_master;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Beast on 10/19/2015.
  */
 public class LoginFragment extends Fragment {
+
+    protected Button mBtnSignupFaceBook;
+    protected ParseUser parseUser;
+    protected String name;
+    protected String lastname;
+    protected String firstname;
+    protected String email;
 
     @Nullable
     @Override
@@ -30,10 +59,80 @@ public class LoginFragment extends Fragment {
             }
 
         });
-
-
-
-
+        InitializeEventListeners(rootView);
         return rootView;
     }
+
+    private void InitializeEventListeners(final View view) {
+        mBtnSignupFaceBook = (Button) view.findViewById(R.id.btnfacebook_login);
+
+        //listen to register button click
+        mBtnSignupFaceBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //toast something
+
+                final List<String> permissions = new ArrayList<String>();
+                permissions.add("public_profile");
+                permissions.add("user_status");
+                permissions.add("user_friends");
+                permissions.add("email");
+                permissions.add("user_birthday");
+
+                Toast.makeText(view.getContext(), "User trying to sign up through Facebook!", Toast.LENGTH_LONG).show();
+
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginFragment.this.getActivity(), permissions, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException err) {
+                        if (user == null) {
+                            Toast.makeText(view.getContext(), "Error: " + err, Toast.LENGTH_LONG).show();
+                            Log.d("MyApp", "Error: " + err);
+                        } else {
+                            if (user.isNew()) {
+                                getUserDetailsFromFB();
+                                Toast.makeText(view.getContext(), "User signed up and logged in through Facebook!", Toast.LENGTH_LONG).show();
+                            } else {
+                                getUserDetailsFromFB();
+                                Toast.makeText(view.getContext(), "User logged in through Facebook!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void getUserDetailsFromFB() {
+
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            name = object.getString("name");
+                            lastname = object.getString("last_name");
+                            firstname = object.getString("first_name");
+                            email = object.getString("email");
+                            saveNewUser();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,last_name,link,email,first_name");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void saveNewUser() {
+        parseUser = ParseUser.getCurrentUser();
+        parseUser.put("first_name", firstname);
+        parseUser.put("last_name", lastname);
+        parseUser.setEmail(email);
+        parseUser.saveInBackground();
+    }
+
 }
--- app/src/main/java/com/shadow/beast/shadow_master/LoginSelectionActivity.java
+++ app/src/main/java/com/shadow/beast/shadow_master/LoginSelectionActivity.java
@@ -4,22 +4,47 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.shadow.beast.apptourlibrary.AppTour;
 import com.shadow.beast.apptourlibrary.MaterialSlide;
 
-public class LoginSelectionActivity extends AppTour {
-        @Override
-        public void init(Bundle savedInstanceState) {
+import java.util.ArrayList;
+import java.util.List;
 
-            int firstColor = Color.parseColor("#0097A7");
-            int secondColor = Color.parseColor("#FFA000");
-            int thirdColor = Color.parseColor("#FF5722");
-            int forthColor = Color.parseColor("#673AB7");
-            int customSlideColor = Color.parseColor("#009866");
+public class LoginSelectionActivity extends AppTour {
+
+    protected LoginFragment loginFragment;
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "0cJSz075MogqVzprRn36GXO6m1ur547EN8fVhOF4", "nafYF4kwB41R4AyTGZY7j0oGPKMVh5DXNzxurWjt");
+        ParseFacebookUtils.initialize(this);
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
+        int firstColor = Color.parseColor("#0097A7");
+        int secondColor = Color.parseColor("#FFA000");
+        int thirdColor = Color.parseColor("#FF5722");
+        int forthColor = Color.parseColor("#673AB7");
+        int customSlideColor = Color.parseColor("#009866");
 
             //Create pre-created fragments
             Fragment firstSlide = MaterialSlide.newInstance(R.drawable.com_shadow_device, "Presentations on the go",
@@ -42,10 +67,12 @@ public class LoginSelectionActivity extends AppTour {
             setSkipButtonTextColor(Color.WHITE);
             setNextButtonColorToWhite();
             setDoneButtonTextColor(Color.WHITE);
-            ShowCustomBottomPanel(new LoginFragment());
+            loginFragment = new LoginFragment();
+            ShowCustomBottomPanel(loginFragment);
 
-
+            //Button mBtnSignupFaceBook = (Button)loginFragment.getActivity().findViewById(R.id.btnfacebook_login);
         }
+
 
         @Override
         public void onSkipPressed() {
@@ -64,4 +91,13 @@ public class LoginSelectionActivity extends AppTour {
             //E.x: Finish the intro.
             finish();
         }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
     }
